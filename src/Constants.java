3befc55cc1aeea5c8b360e2e73e1fbf778fe57d2
said.ozgat <said.ozgat@hotmail.com>
2021-08-28 22:25:19 +0300

    +Constants

---
+++ src/Constants.java
@@ -0,0 +1,14 @@
+/**
+ * Değeri değiştirilemeyen değişkenlere sabit (constant) denir.
+ *
+ * Sabitlerin değişkenlerden iki temel farkı vardır:
+ *  Bir değişkeni sabit yapmak istiyorsanız final belirteci ile tanımlamalısınız.
+ *  Sabitlerin değeri sonradan değiştirilemediği için tanımladığınız anda değer atamalısınız.
+ */
+public class Constants {
+
+    final double pi = 3.14;
+
+    //Error: Variable 'x' might not have been initialized
+    //final byte x;
+}
